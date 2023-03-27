357eb439f658232c3a3f641fe1b5668518cd446c
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-03-27 23:34:29 +0300

    create function that analyse and return the performance of model

--- cv.ani.r
+++ cv.ani.r
@@ -6,3 +6,10 @@ data(Sonar)
 set.seed(123)
 k <- 5
 folds <- sample(rep(1:k, length.out = nrow(Sonar)))
+
+fit_model <- function(train, test) {
+  model <- glm(Class ~ ., data = train, family = binomial)
+  prob <- predict(model, test, type = "response")
+  pred <- ifelse(prob > 0.5, "R", "M")
+  return(list(pred = pred, obs = test$Class))
+}
